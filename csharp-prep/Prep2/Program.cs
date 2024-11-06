2793c96df460ad3bfe3c1dc57efb0de3797126b6
CarlosMedinaSullca <carlos.medina.sullca@gmail.com>
2024-11-06 18:45:15 -0400

    commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        int remainder = grade % 10;
+
+        string sign = " ";
+        
+        string letter = " "; 
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (remainder >= 7 && grade < 97 && grade > 60) 
+        {
+            sign = "+";
+        }
+        else if (remainder < 3 && grade < 97 && grade > 60)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = " ";
+        }
+
+        Console.WriteLine($"Your letter grade is {sign}{letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Explore ways to improve and try againg the next semester!");
+        }
+
+
+
+
     }
 }
