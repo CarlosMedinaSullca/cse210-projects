6781312b1c695b00119238c8676334b42cd1729f
CarlosMedinaSullca <carlos.medina.sullca@gmail.com>
2024-12-07 20:14:59 -0400

    scripture memorizer second approach

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Reference r1 = new Reference("Job", 3, 3);
-        Reference r2 = new Reference("Job", 3, 3, 4);
+        
 
         // TESTING THE REFERENCEC CLASS
         // Console.WriteLine(r1.GetDisplayText());
@@ -17,12 +17,24 @@ class Program
         // w1.Hide();
         // Console.WriteLine(w1.GetDisplayText());
         // w1.Show(); 
-        // Console.WriteLine(w1.GetDisplayText());    
+        // Console.WriteLine(w1.GetDisplayText()); 
+        Console.Write("Enter the name of the book");
+        string book = Console.ReadLine();
+        Console.Write("Enter the chapter of the book: ");
+        int chapter = int.Parse(Console.ReadLine());
+        Console.Write("Enter the first verse: ");
+        int verse1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter the last verse or just press enter if there is only one verse: ");
+        int verse2 = int.Parse(Console.ReadLine());
 
+        Console.Write("Enter the verse or verses");
+        string verses = Console.ReadLine();
+
+        Reference r2 = new Reference(book, chapter, verse1, verse2);
 
         string script1= "Perezca el día en que yo nací y la noche en que se dijo: Un varón ha sido concebido. Sea aquel día sombrío, y no cuide de él Dios desde arriba ni claridad sobre él resplandezca.";
 
-        Scripture s1 = new Scripture( r2, script1);
+        Scripture s1 = new Scripture( r2, verses);
         
         Console.WriteLine(s1.GetDisplayText());
 
