da02aeb89b81b1fc4af3da5639cbf785b29ef4a3
CarlosMedinaSullca <carlos.medina.sullca@gmail.com>
2024-12-17 16:59:45 -0400

    Learning06 activity completed

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        Square s = new Square("violet", 5);
+
+        // Console.WriteLine(s.GetColor());
+        // Console.WriteLine(s.GetArea());
+
+        Rectangle r = new Rectangle("yellow", 4,5);
+        // Console.WriteLine(r.GetColor());
+        // Console.WriteLine(r.GetArea());
+
+        Circle c = new Circle("white", 7);
+
+        // Console.WriteLine(c.GetColor());
+        // Console.WriteLine(c.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(s);
+        shapes.Add(r);
+        shapes.Add(c);
+
+        foreach (Shape i in shapes)
+        {
+            Console.WriteLine(i.GetColor());
+            Console.WriteLine(i.GetArea());
+        }
+
     }
 }
